92ecf1d572f4948ff2f0652f1eb2f7191ea8c64f
ingridrauany <ingid.rauany@hotmail.com>
2024-03-09 12:49:54 -0300

    Configure eslint and prettier, and setup a reset css

--- social-links-profile/vite.config.ts
+++ social-links-profile/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@public": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
   plugins: [react()],
-})
+});
