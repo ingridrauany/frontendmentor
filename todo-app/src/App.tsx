ee54cb8e18e76806d23767e0094c769325a88877
ingridrauany <ingrid.rauany@hotmail.com>
2024-04-16 21:47:06 -0300

    Basic logic

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,5 +1,49 @@
-function App() {
-  return <h1>Hello</h1>;
-}
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+type Todo = {
+  id: string;
+  name: string;
+  isComplete: boolean;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const value: string = e.currentTarget.value;
+      const updatedTodos: Todo[] = [
+        ...todos,
+        {
+          id: uuid(),
+          name: value,
+          isComplete: false,
+        },
+      ];
+      setTodos(updatedTodos);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        name="name"
+        type="text"
+        placeholder="Digite o texto"
+        onKeyDown={handleKeyPress}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
